fdc98478fa489b4f9a6da403b130df2306c48264
mf <mf@tt4.at>
2016-02-08 15:06:25 +0100

    Inspect-view: output classes-summary, nex/prev debate link

--- offenesparlament/op_inspect/views.py
+++ offenesparlament/op_inspect/views.py
@@ -13,6 +13,7 @@ def index(request, value=None):
     debate = False
     statements = []
     llp = None
+    stats = {'text':{}, 'speaker':{}}
     if 'llpnr' in request.GET:
         llp = models.LegislativePeriod.objects.get(
             number=int(request.GET['llpnr']))
@@ -23,22 +24,26 @@ def index(request, value=None):
           llp=llp,
           nr=int(request.GET['debate_nr']))
         for s in  debate.debate_statements.order_by('index').all():
-            links = Selector(text=s.raw_text).xpath('.//a')
+            # links = Selector(text=s.raw_text).xpath('.//a')
             statements.append({
-                "links": links,
+                #"links": links,
                 "model": s,
                 "orig": mark_safe(s.raw_text),
                 "annotated": mark_safe(s.annotated_text),
                 "clean": s.full_text,
             })
 
+            stats['speaker'][s.speaker_role] = stats['speaker'].setdefault(s.speaker_role, 0) + 1
+            stats['text'][s.text_type] = stats['text'].setdefault(s.text_type, 0) + 1
+
     return render(request, 'inspect.html',
                   context={'llps':llps,
                            'show_orig': ('show_orig' in request.GET\
                                          and request.GET['show_orig']),
                            'llpnr': llp.number if llp else False,
-                           'debates': sorted(debates),
+                           'debates': sorted(debates, key=lambda d: d.nr),
                            'debate': debate,
+                           'stats': stats,
                            'statements': statements})
 
 
